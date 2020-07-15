3fd94e41daacfb64692fe9d5942d62adcf4144b6
Curtis Adams <curtisadams28@gmail.com>
2020-07-15 14:55:30 +1200

    fixed scroll bar issues

--- js/map.js
+++ js/map.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-  $("#flip").click(function(){
+$(document).ready(function () {
+  $("#flip").click(function () {
     $("#panel").slideDown("slow");
   });
 });
-
 
 let data;
 let yearquery1 = 1800;
 let yearquery2 = 1820;
 let query = [];
-const key = 'pk.eyJ1IjoiY3VydHV4ZGVsdXhlIiwiYSI6ImNqd2s0MmZpZTBjajQ0OG9lZjQ1cWswbzIifQ.qqwt65rirh2anE7ykAn2hw'
+const key =
+  "pk.eyJ1IjoiY3VydHV4ZGVsdXhlIiwiYSI6ImNqd2s0MmZpZTBjajQ0OG9lZjQ1cWswbzIifQ.qqwt65rirh2anE7ykAn2hw";
 
 let myMap;
 let canvas;
-const mappa = new Mappa('MapboxGL', key);
+const mappa = new Mappa("MapboxGL", key);
 
 const options = {
   lat: 0,
@@ -21,42 +21,42 @@ const options = {
   zoom: 2,
   studio: true, // false to use non studio styles
   //style: 'mapbox.dark' //streets, outdoors, light, dark, satellite (for nonstudio)
-  style: 'mapbox://styles/mapbox/light-v9',
+  /**
+   * mapbox://styles/mapbox/outdoors-v11
+   * mapbox://styles/mapbox/light-v9
+   */
+  style: "mapbox://styles/mapbox/light-v9",
   compact: true,
-}
-
+};
 
 function preload() {
-  data = loadJSON('data/eruption_data.json');
+  data = loadJSON("data/eruption_data.json");
 }
 function setup() {
-
   // Creates the canvas and overlays it on the map
-  canvas = createCanvas(windowWidth,windowHeight - 30);
+  canvas = createCanvas(windowWidth, windowHeight - 30);
+
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
 
-  fill('#E6B199');
-  stroke('#505050');
+  fill("#E6B199");
+  stroke("#505050");
   myMap.onChange(drawPoint);
 
   cursor(ARROW);
-
 }
 
-function draw() {
-
-}
+function draw() {}
 
 // Checks to see if the mouse is clicked on the recorded position of a volcano.
 function mouseClicked() {
-  var clickedvol = [];
-  var max = {Year: 0};
-  for (var i = 0; i < query.length; i++) {
-    var volcano = myMap.latLngToPixel(query[i].Latitude, query[i].Longitude);
-    var normvei = norm(query[i].VEI, 0, 8);
-    var circlesize = normvei * 50;
-    var distance = dist(mouseX, mouseY, volcano.x, volcano.y);
+  let clickedvol = [];
+  let max = {Year: 0};
+  for (let i = 0; i < query.length; i++) {
+    let volcano = myMap.latLngToPixel(query[i].Latitude, query[i].Longitude);
+    let normvei = norm(query[i].VEI, 0, 8);
+    let circlesize = normvei * 50;
+    let distance = dist(mouseX, mouseY, volcano.x, volcano.y);
     if (distance <= circlesize) {
       //console.log(query[i]);
       clickedvol.push(query[i]);
@@ -69,41 +69,40 @@ function mouseClicked() {
   on the top.
   */
 
-  for (var i = 0; i < clickedvol.length; i++) {
+  for (let i = 0; i < clickedvol.length; i++) {
     if (clickedvol[i].Year >= max.Year) {
       max = clickedvol[i];
     }
   }
   if (max.Year == 0) {
     max = null;
-  }
-  else {
+  } else {
     currentView(max);
   }
-
-
 }
 
 // Draws the circles onto the canvas using data from the jsonToArray() method.
-function drawPoint(){
+function drawPoint() {
   clear();
   jsonToArray();
   //console.log(query);
-  for (var i = 0; i < query.length; i++) {
-    var volcano = myMap.latLngToPixel(query[i].Latitude, query[i].Longitude);
-    var normvei = norm(query[i].VEI, 0, 8);
-    var circlesize = normvei * 50;
+  for (let i = 0; i < query.length; i++) {
+    let volcano = myMap.latLngToPixel(query[i].Latitude, query[i].Longitude);
+    let normvei = norm(query[i].VEI, 0, 8);
+    let circlesize = normvei * 50;
     ellipse(volcano.x, volcano.y, circlesize, circlesize);
   }
-
 }
 
 // Takes the data from the JSON file and pushes it to query[] based on the year.
 function jsonToArray() {
   query = [];
 
-  for (var i = 0; i < data.eruptions.length; i++) {
-    if (data.eruptions[i].Year >= yearquery1 && data.eruptions[i].Year <= yearquery2) {
+  for (let i = 0; i < data.eruptions.length; i++) {
+    if (
+      data.eruptions[i].Year >= yearquery1 &&
+      data.eruptions[i].Year <= yearquery2
+    ) {
       //console.log(data.eruptions[i]);
       query.push(data.eruptions[i]);
     }
@@ -113,21 +112,21 @@ function jsonToArray() {
 // Function to set top info section to the values of the selected volcano.
 function currentView(currentvol) {
   console.log(currentvol);
-  document.getElementById('volcano').innerHTML = currentvol.Name;
-  document.getElementById('country').innerHTML = currentvol.Country;
-  document.getElementById('vei').innerHTML = currentvol.VEI;
-  document.getElementById('type').innerHTML = currentvol.Type;
-  document.getElementById('lat').innerHTML = currentvol.Latitude;
-  document.getElementById('long').innerHTML = currentvol.Longitude;
-  document.getElementById('year').innerHTML = currentvol.Year;
-  document.getElementById('descp').innerHTML = currentvol.Desc;
+  document.getElementById("volcano").innerHTML = currentvol.Name;
+  document.getElementById("country").innerHTML = currentvol.Country;
+  document.getElementById("vei").innerHTML = currentvol.VEI;
+  document.getElementById("type").innerHTML = currentvol.Type;
+  document.getElementById("lat").innerHTML = currentvol.Latitude;
+  document.getElementById("long").innerHTML = currentvol.Longitude;
+  document.getElementById("year").innerHTML = currentvol.Year;
+  document.getElementById("descp").innerHTML = currentvol.Desc;
 
-  document.getElementById('infobar').style.display = 'initial';
-  document.getElementById('flip').style.display = 'initial';
+  document.getElementById("infobar").style.display = "initial";
+  document.getElementById("flip").style.display = "initial";
 }
 
 function closeInfo() {
-  document.getElementById('infobar').style.display = 'none';
-  document.getElementById('flip').style.display = 'none';
-  document.getElementById('panel').style.display = 'none';
+  document.getElementById("infobar").style.display = "none";
+  document.getElementById("flip").style.display = "none";
+  document.getElementById("panel").style.display = "none";
 }
--- js/slider.js
+++ js/slider.js
@@ -1,94 +1,218 @@
 // Listens for the mouse to be pressed down and then runs the mouseDown function.
 document.getElementById("slidertab1").addEventListener("mousedown", mouseDown);
 document.getElementById("slidertab2").addEventListener("mousedown", mouseDown);
-var varmouse = true;
-var sliderid;
-var slidertab1 = 0;
-var slidertab2 = 200;
+let varmouse = true;
+let sliderid;
+let slidertab1 = 0;
+let slidertab2 = 124;
+let barwidth = 124;
+let sliderLimit = false;
 
-// runs the mouseMove function when the mouse moves sets varmoues to false when
+document.getElementById("slidertab1").style.left = slidertab1 + "px";
+document.getElementById("slidertab2").style.left = slidertab2 + "px";
+
+// runs the mouseMove function when the mouse moves sets varmouse to false when
 // the mouse is unpressed.
-function mouseDown(e) {
-  sliderid = this.id.toString();
+function mouseDown() {
+  sliderId = this.id.toString();
+
   varmouse = true;
-  document.addEventListener('mousemove', mouseMove);
-  document.addEventListener("mouseup", function (e) {
+  document.addEventListener("mousemove", mouseMove);
+  document.addEventListener("mouseup", function () {
     varmouse = false;
   });
 }
 
 // Sets the position of the slider based on the x coordinates of the mouse.
 function mouseMove(e) {
-  var sliderpos = e.x;
-  sliderRules();
-  document.getElementById(sliderid).style.left = sliderpos;
+  let sliderpos = cursorRules(e.x, sliderId);
 
+  if (sliderId === "slidertab2" && sliderpos <= slidertab1 + 90) {
+    sliderpos = slidertab1 + 90;
+  }
+  if (sliderId === "slidertab1" && sliderpos >= slidertab2 - 90) {
+    sliderpos = slidertab2 - 90;
+  }
+
+  //document.getElementById(sliderid).style.left = sliderpos;
 
   // If varmouse is false, the slider will no longer move.
   if (varmouse == false) {
-    document.removeEventListener('mousemove', mouseMove);
-
+    document.removeEventListener("mousemove", mouseMove);
   }
 
+  let posnorm = norm(sliderpos, 0, window.innerWidth - 8);
+  let year = Math.floor(1800 + posnorm * 215);
 
-  //var posnorm = 2015 - (norm(sliderpos, 0, window.innerWidth - 8) * 6375);
-  //var posnorm = Math.floor(2015 - ((1 - (norm(sliderpos, 0, window.innerWidth - 8))) * 6375));
+  //let year = getYearFromPos(sliderpos);
+
+  setSliderPos(sliderpos, sliderId);
+
+  let yearId;
+  if (sliderId === "slidertab1") {
+    yearId = "year1";
+    slidertab1 = sliderpos;
+  } else {
+    yearId = "year2";
+    slidertab2 = sliderpos;
+  }
+  setSliderYear(sliderpos, yearId, year);
+  setSliderBar();
+}
+
+// General rules for preventing unwanted positioning behaviour for the sliders.
+function cursorRules(cursorX, sliderId) {
+  // Stops slider from going off the screen.
+  if (cursorX < 0) {
+    cursorX = 0;
+  }
+  if (cursorX > window.innerWidth - 8) {
+    cursorX = window.innerWidth - 8;
+    sliderLimit = true;
+  }
   /*
-  var posnorm = norm(Math.log(sliderpos), 0, Math.log(window.innerWidth - 8));
-  var norminverse = 1 - posnorm;
-  var yeardec = 2015 - (norminverse * 6375);
-  var year = Math.floor(yeardec);
 
-  if (sliderpos == 0) {
-    sliderpos = 1;
+  if (sliderId === "slidertab2" && cursorX < slidertab1 + 90) {
+    cursorX = slidertab1 + 90;
+  }
+  if (sliderId === "slidertab1" && cursorX > slidertab2 - 90) {
+    cursorX = slidertab1 - 90;
   }
   */
 
-  var posnorm = norm(sliderpos, 0, window.innerWidth - 8);
+  /*
+  if ((sliderId = "slidertab1")) {
+    console.log(slidertab1, cursorX, sliderId);
+  }
+  if ((sliderId = "slidertab2")) {
+    console.log(slidertab2, cursorX, sliderId);
+  }
+  */
 
-  //var norminverse = 1 - posnorm;
+  /*
+  else {
+    sliderLimit = false;
+  }
+  console.log(sliderLimit);
 
-  var yeardec = 1800 + (posnorm * 215);
-  var year = Math.floor(yeardec);
+  if (sliderId === "slidertab1" && sliderLimit === true) {
+    console.log("limit reached");
 
+    cursorX = slidertab2 - barwidth;
+  }
+  */
 
-  // Sets the yearquery, moves the slider tab and sets the number above the tab.
-  if (sliderid == 'slidertab1') {
-    slidertab1 = sliderpos;
+  // Stops the sliders from getting too close to each other.
+  /*
+  if (sliderId === "slidertab1" && cursorX > slidertab2 - 90) {
+    cursorX = slidertab2 - 90;
+  }
+
+  if (sliderId === "slidertab2" && cursorX < slidertab1 + 90) {
+    cursorX = slidertab1 + 90;
+  }
+
+  */
+  return cursorX;
+}
+
+function setSliderPos(cursorX, sliderTab) {
+  drawPoint();
+  /*
+  if (sliderTab === "slidertab1" && cursorX > slidertab2 - 100) {
+    console.log("gone over");
+    cursorX = slidertab2 - 100;
+  }
+  */
+  /*
+  if (sliderTab === "slidertab1" && cursorX > window.innerWidth - 200) {
+    console.log("met");
+
+    cursorX = window.innerWidth - 200;
+  }
+  */
+  document.getElementById(sliderTab).style.left = cursorX + "px";
+}
+
+function setSliderYear(cursorX, textId, year) {
+  document.getElementById(textId).innerHTML = year;
+  document.getElementById(textId).style.left = cursorX - 15 + "px";
+  if (cursorX <= 18) {
+    document.getElementById(textId).style.left = cursorX + "px";
+  }
+  if (cursorX >= window.windowWidth - 23) {
+    document.getElementById(textId).style.left = cursorX - 30 + "px";
+  }
+  if (textId === "year1") {
     yearquery1 = year;
-    drawPoint();
-    document.getElementById('year1').innerHTML = year;
-    document.getElementById('year1').style.left = (sliderpos - 15) + 'px';
-    if (sliderpos <= 18) {
-      document.getElementById('year1').style.left = (sliderpos) + 'px';
-    }
-
-  }
-  if (sliderid == 'slidertab2') {
-
-    slidertab2 = sliderpos;
+  } else {
     yearquery2 = year;
-    drawPoint();
-    document.getElementById('year2').innerHTML = year;
-    document.getElementById('year2').style.left = (sliderpos - 12) + 'px';
-    if (sliderpos >= window.windowWidth - 23) {
-      document.getElementById('year2').style.left = (sliderpos - 30) + 'px';
-    }
-
   }
-  var barwidth = slidertab2 - slidertab1;
-  document.getElementById('sliderbar').style.width = barwidth + 'px';
-  document.getElementById('sliderbar').style.left = slidertab1 + 4 + 'px';
+}
+function setSliderBar() {
+  barwidth = slidertab2 - slidertab1;
+  document.getElementById("sliderbar").style.width = barwidth + "px";
+  document.getElementById("sliderbar").style.left = slidertab1 + 4 + "px";
+}
 
+let barlengthToCursor;
+document.getElementById("sliderbar").addEventListener("mousedown", sliderClick);
 
-  function sliderRules() {
-    if (sliderpos < 0) {
-      sliderpos = 0;
-    }
+function sliderClick(e) {
+  let mouseX = e.x;
+  // Calculates the length from the left slider tab to the x position of the cursor.
+  barlengthToCursor = mouseX - slidertab1;
 
-    if (sliderpos > window.innerWidth - 8) {
-      sliderpos = window.innerWidth - 8;
-    }
+  document.addEventListener("mousemove", barMove);
+}
+function barMove(e) {
+  // Calculates how far left the bar needs to be from the position of the cursor.
 
+  //let mouseX = cursorRules(e.x);
+
+  let sliderPosLeft = cursorRules(e.x - barlengthToCursor, "slidertab1");
+  let sliderPosRight = cursorRules(
+    sliderPosLeft + slidertab2 - slidertab1,
+    "slidertab2"
+  );
+
+  let sliderCheck;
+  let newbarwidth = sliderPosRight - sliderPosLeft;
+
+  if (sliderPosLeft + slidertab2 - slidertab1 > window.innerWidth - 8) {
+    console.log("over");
+
+    sliderPosLeft = sliderPosRight - newbarwidth;
   }
+  /*
+  if (sliderPosRight > window.innerWidth - 8) {
+    sliderPosLeft = sliderPosRight - barwidth;
+  }
+  */
+
+  /*
+  console.log("sliderleft", sliderPosLeft);
+  console.log("sliderright", sliderPosRight);
+  console.log("barwidth", barwidth);
+  */
+
+  document.getElementById("sliderbar").style.left = sliderPosLeft + "px";
+  document.addEventListener("mouseup", sliderClickUp);
+
+  setSliderPos(sliderPosLeft, "slidertab1");
+  slidertab1 = sliderPosLeft;
+  setSliderPos(sliderPosRight, "slidertab2");
+  slidertab2 = sliderPosRight;
+
+  setSliderYear(slidertab1, "year1", getYearFromPos(sliderPosLeft));
+  setSliderYear(slidertab2, "year2", getYearFromPos(sliderPosRight));
+}
+
+function sliderClickUp() {
+  document.removeEventListener("mousemove", barMove);
+}
+function getYearFromPos(sliderX) {
+  let posnorm = norm(sliderX, 0, window.innerWidth - 8);
+  let year = Math.floor(1800 + posnorm * 215);
+  return year;
 }
